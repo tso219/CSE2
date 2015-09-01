0edf7bffa96ad947691d0f01f8c0d4766557a0a6
tso219 <tso219@lehigh.edu>
2015-09-01 20:03:47 +0000

    updated HelloWorld

---
+++ lab01-HelloWorld/HelloWorld.java
@@ -0,0 +1,25 @@
+//define the class
+public class HelloWorld{
+//add main method
+    public static void main(String[] args) {
+//    {int a=0;
+        
+//        for(a=0;a<40;a+=1){
+//    if (a%2==0)
+//    System.out.print("Hello");
+//    else if(a%2==1)
+//    System.out.println("World");
+//}
+//}
+
+//print the statement with a box around it
+    System.out.println("##############################");
+    System.out.println("##############################");
+    System.out.println("#                            #");
+    System.out.println("#   Hello World 2014!!       #");
+    System.out.println("#                            #");
+    System.out.println("##############################");
+    System.out.println("##############################");
+    System.out.println("test");
+    }
+}
