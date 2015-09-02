d5f6c1da1e63110d20cac83c65dc99e14084288a
tso219 <tso219@lehigh.edu>
2015-09-02 01:09:03 +0000

    my 1st hw on cse2

---
+++ lab01-HelloWorld/hw01/WelcomeClass.java
@@ -0,0 +1,21 @@
+//class name
+public class WelcomeClass{
+//main method
+public static void main(String []args){
+
+    //make java say "welcome tso219" and something about me
+    System.out.println("    -----------");
+    System.out.println("     |WELCOME|");
+    System.out.println("    -----------");
+    System.out.println("  ^  ^  ^  ^  ^  ^");
+    System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\ ");
+    System.out.println("<-T--S--O--2--1--9->");
+    System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ /");
+    System.out.println("  v  v  v  v  v  v");
+    //short autobiography
+    System.out.println("A week ago I joined LU philarmornic.");
+    System.out.println("It is certainly better than my high school");
+    System.out.println("orchestra, but it still fails to meet");
+    System.out.println("my ear's perfect pitch standards.");
+}
+}
